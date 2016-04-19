149b3d61cecea62988d14f868af9967c34aa8712
Benj Sicam <bsicam@reekoh.com>
2016-04-20 00:58:29 +0800

    Build Error Fix

--- test/oracle-storage.test.js
+++ test/oracle-storage.test.js
@@ -56,7 +56,7 @@ describe('Storage', function () {
 						password: PASSWORD,
 						schema: SCHEMA,
 						table: TABLE,
-						fields: JSON.stringify({
+						field_mapping: JSON.stringify({
 							id: {source_field: 'id', data_type: 'Integer'},
 							co2_field: {source_field: 'co2', data_type: 'String'},
 							temp_field: {source_field: 'temp', data_type: 'Integer'},
